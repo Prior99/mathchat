8f56fddf4a3f714cec11f07cdfa230ccfe72f4bc
Prior <fgnodtke@cronosx.de>
2014-05-11 21:21:27 +0200

    Initial commit.

---
+++ client/websocket.js
@@ -0,0 +1,59 @@
+var port = 7834;
+
+Websocket = {
+	openStack : [],
+	requests : {},
+	responses : {},
+	counter : 0,
+	connect : function() {
+		var self = this;
+		this.socket = new WebSocket("ws://" + window.location.hostname + ":" + port + "/");
+		this.socket.onopen = function(e) {
+			for(var i = 0; i < self.openStack.length; i++) {
+				self.openStack[i]();
+			}
+		};
+		this.socket.onmessage = function(e)
+		{
+			console.log(e.data);
+			var obj = JSON.parse(e.data);
+			console.log(obj);
+			if(obj._type == "Request") {
+				if(self.requests[obj._requestID] !== undefined) {
+					var answer = self.requests[obj._requestID](obj);
+					if(answer === undefined) answer = {};
+					answer._responseID = obj._responseID;
+					answer._type = "Response";
+					self.socket.send(JSON.stringify(answer));
+				}
+			}
+			else if(obj._type == "Response") {
+				if(self.responses[obj._responseID] !== undefined) {
+					self.responses[obj._responseID](obj);
+				}
+			}
+		}
+	},
+	addOpenListener : function(listener) {
+		this.openStack.push(listener);
+	},
+	addListener : function(request, listener) {
+		this.requests[request] = listener;
+	},
+	send : function(request, obj, listener) {
+		this.responses[this.counter] = listener;
+		obj._requestID = request;
+		obj._type = "Request";
+		obj._responseID = this.counter++;
+		obj = JSON.stringify(obj);
+		this.socket.send(obj);
+	},
+	removeListener : function(request) {
+		this.requests[request] = undefined;
+		console.log("Rmove listener for ");
+	}
+};
+
+$(function() {
+	Websocket.connect();
+});
---
+++ server/server.js
@@ -0,0 +1,63 @@
+var port = 7834;
+
+var WebSocketServer = require('ws').Server;
+var Websocket = require('./websocket');
+
+var clients = [];
+var usernames = [];
+
+(new WebSocketServer({host: '0.0.0.0',port: port})).on('connection', function(ws) {
+	ws = new Websocket(ws);
+	clients.push(ws);
+	ws.addCloseListener(function() {
+		clients.splice(clients.indexOf(ws), 1);
+		usernames.splice(usernames.indexOf(ws.username), 1);
+		console.log("User left", usernames);
+		for(var i = 0; i < clients.length; i++) {
+			var w = clients[i];
+			w.send("Disconnect", {
+				user : ws.username
+			});
+		}
+	});
+	ws.addListener("Username", function(obj) {
+		var okay;
+		if(ws.username == undefined && obj.username.length >= 3 && obj.username.length <= 12) {
+			if(usernames.indexOf(obj.username) == -1) {
+				ws.username = obj.username;
+				for(var i = 0; i < clients.length; i++) {
+					var w = clients[i];
+					w.send("Connect", {
+						user : obj.username
+					});
+				}
+				usernames.push(obj.username);
+				okay = true;
+				console.log("User entered", usernames);
+			}
+			else {
+				okay = false;
+			}
+		}
+		else 
+		okay = false;
+		return {
+			okay: okay
+		}
+	});
+	ws.addListener("Userlist", function() {
+		return {
+			users: usernames
+		};
+	});
+	ws.addListener("Message", function(obj) {
+		if(ws.username === undefined) return;
+		for(var i = 0; i < clients.length; i++) {
+			var w = clients[i];
+			w.send("Message", {
+				user : ws.username,
+				msg : obj.msg
+			});
+		}
+	});
+});
